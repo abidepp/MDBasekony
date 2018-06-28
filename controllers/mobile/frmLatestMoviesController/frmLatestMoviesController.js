e48f316ebd1ea30104783f7e425b31879e5ccdb9
Truetech-PC\Truetech <divya.r@enterprisetouch.co.in>
2018-06-28 18:10:09 +0530

    dev phase commit- two

--- controllers/mobile/frmLatestMoviesController/frmLatestMoviesController.js
+++ controllers/mobile/frmLatestMoviesController/frmLatestMoviesController.js
@@ -1,75 +1,103 @@
 define({ 
 
- //Type your controller code here 
-  
+  //Type your controller code here 
+
   loadFormLatestMovies : function()
   {
-    this.view.postShow = this.populateSegment;
-  	this.view.tabView.gridMovies.onRowSelected = this.getSelectedGridData;
-    
+    try
+    {
+      this.view.postShow = this.populateSegment;
+      this.view.tabView.gridMovies.onRowSelected = this.getSelectedGridData;    
+    }
+    catch(err)
+    {
+      kony.print("loadFormLatestMovies exception"+err);
+    }
   },
-  
+
   getSelectedGridData: function()
   {
-//     var data = this.view.tabView.gridMovies.selectedCellItem;
-//     kony.print("selectedCellItem"+data);
-//     var dataTwo = this.view.tabView.gridMovies.selectedCellIndex;
-//     kony.print("selectedCellIndex"+dataTwo);
-//     var dataThree = this.view.tabView.gridMovies.selectedIndex;
-//     kony.print("selectedIndex"+dataThree);
-//     var dataFour = this.view.tabView.gridMovies.selectedIndices;
-//     kony.print("selectedIndices"+dataFour);
-    let dataFive = this.view.tabView.gridMovies.selectedItem;
-    kony.print("selectedItem"+dataFive);
-//     var dataSix = this.view.tabView.gridMovies.selectedItems;
-//     kony.print("selectedItems"+dataSix);
-    let movieID = dataFive.col3id;
-    if(movieID !== null || movieID !== undefined)
+    
+    try
+    {
+      //     var data = this.view.tabView.gridMovies.selectedCellItem;
+      //     kony.print("selectedCellItem"+data);
+      //     var dataTwo = this.view.tabView.gridMovies.selectedCellIndex;
+      //     kony.print("selectedCellIndex"+dataTwo);
+      //     var dataThree = this.view.tabView.gridMovies.selectedIndex;
+      //     kony.print("selectedIndex"+dataThree);
+      //     var dataFour = this.view.tabView.gridMovies.selectedIndices;
+      //     kony.print("selectedIndices"+dataFour);
+      let dataFive = this.view.tabView.gridMovies.selectedItem;
+      
+      kony.print("selectedItem"+dataFive);
+      //     var dataSix = this.view.tabView.gridMovies.selectedItems;
+      //     kony.print("selectedItems"+dataSix);
+      let movieID = dataFive.col3id.toString();
+      kony.print("movieID"+movieID);
+      if(movieID !== null || movieID !== undefined)
       {
         var inputParams = {
           api_key : "6c899bbd67713e8f2720b10f00b18807",
-          movieID : movieID,
-  		  language : "en-US"
+          movie_id : movieID,
+          language : "en-US"
         };
         var headers = {};
-        kony.print("invoke service");
-    invokeService("CreditsAndDetails", "getDetailsAndCredits", headers, inputParams, "getDetailsAndCreditsSuccessCallback", "getDetailsAndCreditsFailureCallback", this);
+        kony.print("invoke service getDetailsAndCredits"+JSON.stringify(inputParams));
+        invokeService("CreditsAndDetails", "getDetailsAndCredits", headers, inputParams, "getDetailsAndCreditsSuccessCallback", "getDetailsAndCreditsFailureCallback", this);
       }
-    
+      //MoviesInfo/credits
+    }
+    catch(err)
+    {
+      kony.print("getSelectedGridData exception"+err);
+    }
+
+
   },
-  
+
   getDetailsAndCreditsSuccessCallback : function(response)
   {
     try
-      {
-         kony.print("getDetailsAndCreditsSuccessCallback"+response);
-      }
+    {
+      kony.print("getDetailsAndCreditsSuccessCallback"+JSON.stringify(response));
+      let movieDetails = response;
+      this.view.ImageView.src = CONSTANTS.image_link_500+movieDetails.poster_path;
+      this.view.RTtitle.text = movieDetails.original_title + "\n" + movieDetails.tagline;
+      this.view.lblRating.text = "Rating :"+movieDetails.vote_average;
+      this.view.lblOverview.text = movieDetails.overview;
+      this.view.Imageone.src = CONSTANTS.image_link_profile+movieDetails.profile_path;
+      this.view.Imagetwo.src = CONSTANTS.image_link_profile+movieDetails.profile_path;
+      this.view.Imagethree.src = CONSTANTS.image_link_profile+movieDetails.profile_path;
+      
+      this.view.FlexMoviePopup.isVisible = true;
+    }
     catch(err)
-      {
-        kony.print("getDetailsAndCreditsSuccessCallback exception"+err);
-      }
+    {
+      kony.print("getDetailsAndCreditsSuccessCallback exception"+err);
+    }
   },
-  
+
   getDetailsAndCreditsFailureCallback : function(error)
   {
     try
-      {
-        kony.print("getDetailsAndCreditsFailureCallback"+error);
-      }
+    {
+      kony.print("getDetailsAndCreditsFailureCallback"+JSON.stringify(error));
+    }
     catch(err)
-      {
-        kony.print("getDetailsAndCreditsFailureCallback exception"+err);
-      }
+    {
+      kony.print("getDetailsAndCreditsFailureCallback exception"+err);
+    }
   },
-  
+
   populateSegment: function()
   {
     kony.print("---->populate segment called");
-//     this.view.SegmentMovies.widgetDataMap = {
-//       "lblID":"lblID",
-//       "lbl":"lbl",
-//       "img": "img"
-//     };
+    //     this.view.SegmentMovies.widgetDataMap = {
+    //       "lblID":"lblID",
+    //       "lbl":"lbl",
+    //       "img": "img"
+    //     };
     var headers = {};
     var inputParams = {
       api_key : "6c899bbd67713e8f2720b10f00b18807",
@@ -79,63 +107,63 @@ define({
     kony.print("---->calling invoke service");
     invokeService("popularAll", "popularMoviesTV", headers, inputParams, "loadSegmentSuccessCallBack", "loadSegmentFailureCallback", this);
   },
-  
+
   loadSegmentSuccessCallBack: function (response)
   {
     kony.print("loadSegmentSuccessCallBack response--->"+JSON.stringify(response));
-//   	this.view.errorMessage.lblMessageText = response;
-    
-    
+    //   	this.view.errorMessage.lblMessageText = response;
+
+
     var movieDetails = [];
     var movie_posters = [];
     var show_posters = [];
     movieDetails = response.results;
     kony.print("loadSegmentSuccessCallBack"+movieDetails.length);
-    
+
     for(var i = 0; i < movieDetails.length-2; i+=3)
+    {
+      var moviesData= {};
+      var tvshowData= {};
+      if(movieDetails[i].title !== undefined)
       {
-        var moviesData= {};
-        var tvshowData= {};
-        if(movieDetails[i].title !== undefined)
-        {
-          moviesData = 
-            {
-            "col1" : CONSTANTS.image_link+movieDetails[i].poster_path,
-            "col1id" : movieDetails[i].id,
-            "col2" : CONSTANTS.image_link+movieDetails[i+1].poster_path,
-            "col2id" : movieDetails[i].id,
-            "col3" : CONSTANTS.image_link+movieDetails[i+2].poster_path,
-            "col3id" : movieDetails[i].id
-          };  
-           movie_posters.push(moviesData);
-        }
-        else if(movieDetails[i].name !== undefined)
-        {
-          tvshowData = 
-            {
-            "col1" : CONSTANTS.image_link+movieDetails[i].poster_path,
-            "col1id" : movieDetails[i].id,
-            "col2" : CONSTANTS.image_link+movieDetails[i+1].poster_path,
-            "col2id" : movieDetails[i].id,
-            "col3" : CONSTANTS.image_link+movieDetails[i+2].poster_path,
-            "col3id" : movieDetails[i].id
-          };
-          show_posters.push(tvshowData); 
-        }
-   
+        moviesData = 
+          {
+          "col1" : CONSTANTS.image_link+movieDetails[i].poster_path,
+          "col1id" : movieDetails[i].id,
+          "col2" : CONSTANTS.image_link+movieDetails[i+1].poster_path,
+          "col2id" : movieDetails[i].id,
+          "col3" : CONSTANTS.image_link+movieDetails[i+2].poster_path,
+          "col3id" : movieDetails[i].id
+        };  
+        movie_posters.push(moviesData);
       }
-    
-    
-//     this.view.SegmentMovies.setData(data);
-   
+      else if(movieDetails[i].name !== undefined)
+      {
+        tvshowData = 
+          {
+          "col1" : CONSTANTS.image_link+movieDetails[i].poster_path,
+          "col1id" : movieDetails[i].id,
+          "col2" : CONSTANTS.image_link+movieDetails[i+1].poster_path,
+          "col2id" : movieDetails[i].id,
+          "col3" : CONSTANTS.image_link+movieDetails[i+2].poster_path,
+          "col3id" : movieDetails[i].id
+        };
+        show_posters.push(tvshowData); 
+      }
+
+    }
+
+
+    //     this.view.SegmentMovies.setData(data);
+
     this.view.tabView.gridMovies.addAll(movie_posters);
     this.view.tabView.gridTvShows.addAll(show_posters);
     kony.print("grid data data set successfully");
-},
-  
+  },
+
   loadSegmentFailureCallback: function (err)
   {
- 	kony.print("loadSegmentFailureCallback"+err); 
-}
+    kony.print("loadSegmentFailureCallback"+err); 
+  }
 
- });
+});
--- modules/constants.js
+++ modules/constants.js
@@ -2,5 +2,47 @@
 
 var CONSTANTS = 
     {
-      image_link:"http://image.tmdb.org/t/p/w342/"
+      image_link:"http://image.tmdb.org/t/p/w342/",
+      image_link_banner : "http://image.tmdb.org/t/p/w500/",
+      image_link_profile : "http://image.tmdb.org/t/p/w185/",
+      video_link : "https://www.youtube.com/watch?v="
     };
+
+/*
+"backdrop_sizes": [
+  "w300",
+  "w780",
+  "w1280",
+  "original"
+],
+"logo_sizes": [
+  "w45",
+  "w92",
+  "w154",
+  "w185",
+  "w300",
+  "w500",
+  "original"
+],
+"poster_sizes": [
+  "w92",
+  "w154",
+  "w185",
+  "w342",
+  "w500",
+  "w780",
+  "original"
+],
+"profile_sizes": [
+  "w45",
+  "w185",
+  "h632",
+  "original"
+],
+"still_sizes": [
+  "w92",
+  "w185",
+  "w300",
+  "original"
+]
+*/
