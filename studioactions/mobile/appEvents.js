a08bdbbe9034ecb5308f633c2fb6a35498ba04fd
Truetech-PC\Truetech <divya.r@enterprisetouch.co.in>
2018-07-05 17:18:50 +0530

    update in the action files

---
+++ studioactions/mobile/appEvents.js
@@ -0,0 +1,10 @@
+define({
+    AS_AppEvents_c18a3d5e044b40769bdaf77e75490137: function AS_AppEvents_c18a3d5e044b40769bdaf77e75490137(eventobject) {
+        var self = this;
+        return registerPhone.call(this);
+    },
+    AS_AppEvents_f8aa91a57ad249ffb4a8f0a7eaa00ca3: function AS_AppEvents_f8aa91a57ad249ffb4a8f0a7eaa00ca3(eventobject) {
+        var self = this;
+        return setupPushCallBacks.call(this);
+    }
+});
